acf3afc17e44165d694432c75ce748007125f3b5
mockee <mockee@gmail.com>
2012-10-30 23:12:18 +0800

    Use comma-first & no semicolon coding style for `define.js`

--- define.js
+++ define.js
@@ -1,141 +1,143 @@
-(function(win) {
-  var each = function(obj, iterator, context) {
-    if (!obj) { return; }
+;(function(win) {
+  function each(obj, iterator, context) {
+    if (!obj) { return }
     if (Array.prototype.forEach && obj.forEach) {
-      obj.forEach(iterator, context);
+      obj.forEach(iterator, context)
     } else {
       for (var i = 0, l = obj.length; i < l; i++) {
         if (i in obj) {
-          iterator.call(context, obj[i], i, obj);
+          iterator.call(context, obj[i], i, obj)
         }
       }
     }
-  },
+  }
 
-  isString = function(obj) {
-    return Object.prototype.toString.call(obj) == '[object String]';
-  };
+  function isString(obj) {
+    return Object.prototype.toString.call(obj) == '[object String]'
+  }
 
   function define(name, deps, fn) {
     if (!fn) {
       if (deps) {
-        fn = deps;
+        fn = deps
       } else {
-        fn = name;
-        name = [];
+        fn = name
+        name = []
       }
 
       // anonymous module
       if (!isString(name)) {
-        deps = name;
-        name = '';
+        deps = name
+        name = ''
       } else {
-        deps = [];
+        deps = []
       }
     }
 
-    var callee = define,
-      len = deps.length,
-      exports = {},
-      mod = callee._global_exports[name] || {},
-      current_ns = callee._ns = mod.ns || 'window';
+    var callee = define
+      , len = deps.length
+      , exports = {}
+      , mod = callee._global_exports[name] || {}
+      , current_ns = callee._ns = mod.ns
+        || callee._current_ns || callee._ns || 'window'
 
     // collect exports of dependent modules
     deps = ((/^function.*?\(([\w'"\$\/\-\:,\n\r\s]*)\)/
       .exec(fn.toString()) || [])[1] || '')
-      .replace(/\s+/g, '').split(',');
+      .replace(/\s+/g, '').split(',')
 
-    deps.length = len;
+    deps.length = len
 
     each(deps, function(name, i) {
       // replace exports with corresponding functions
-      deps[i] = win[current_ns][name] || win[name];
-    });
+      deps[i] = win[current_ns][name] || win[name]
+    })
 
     // add `require`
     deps.push(function(reqs, cb) {
       if (cb) {
-        require(reqs, cb);
+        require(reqs, cb)
       } else {
-        return (callee._global_exports[reqs] || {}).exports;
+        return (callee._global_exports[reqs] || {}).exports
       }
-    });
+    })
 
     // add `exports`
-    deps.push(exports);
+    deps.push(exports)
 
-    exports = mod.exports = fn.apply(this, deps) || exports;
+    exports = mod.exports = fn.apply(this, deps) || exports
 
     if (mod.alias) {
       each(mod.alias, function(name) {
-        name = name.split('.');
-        var context = win, i = name[0];
+        name = name.split('.')
+        var context = win, i = name[0]
         if (name[1]) {
-          context = context[i] = context[i] || {};
-          i = name[1];
+          context = context[i] = context[i] || {}
+          i = name[1]
         }
-        context[i] = exports;
-      });
+        context[i] = exports
+      })
     }
   }
 
   function require(reqs) {
     if (isString(reqs)) {
-      reqs = [reqs];
+      reqs = [reqs]
     }
-    var args = arguments;
-    setTimeout(function(){
-      define.apply(this, args);
-    }, 0);
+    var args = arguments
+    setTimeout(function() {
+      define.apply(this, args)
+    }, 0)
   }
 
   // configuration
-  define._global_exports = {};
+  define._global_exports = {}
 
   // set namespace
   define.ns = function(mid, namespace) {
     // for all modules
     if (!namespace) {
-      this._ns = mid;
+      this._ns = mid
     } else {
       // for specified module
-      var mods = this._global_exports;
+      var mods = this._global_exports
       if (!mods[mid]) {
         mods[mid] = {
           alias: []
-        };
+        }
       }
-      mods[mid].ns = namespace;
+      mods[mid].ns = namespace
     }
-  };
+  }
 
   // set aliases for exports
   define.config = function(mid, vars) {
     if (mid === Object(mid)) {
       for (var id in mid) {
-        this.config(id, mid[id]);
+        this.config(id, mid[id])
       }
-      return;
+      return
     }
 
-    var mods = this._global_exports;
+    var mods = this._global_exports
 
     if (!mods[mid]) {
       mods[mid] = {
-        ns: this._ns,
-        alias: []
-      };
+        ns: this._ns
+      , alias: []
+      }
     }
 
     if (isString(vars)) {
-      vars = [vars];
+      vars = [vars]
     }
 
-    [].push.apply(mods[mid].alias, vars);
-  };
+    [].push.apply(mods[mid].alias, vars)
+  }
 
-  define.amd = { jQuery: true };
+  define.amd = { jQuery: true }
 
-  win.define = define;
+  win.define = define
+  win.require = require
 
-})(window);
+})(window)
