75d375fadfa274f71fd1f3297c65b1bbd2f5c4d0
mockee <mockee@gmail.com>
2012-11-28 14:09:06 +0800

    IE8 can not accept `!important` param

--- mod/matchMedia.js
+++ mod/matchMedia.js
@@ -4,27 +4,29 @@
  * written in the RequireJS module format
  */
 
-define('mod/matchmedia', function() {
+define(function() {
   var bool, doc = document
     , docElem = doc.documentElement
     , refNode = docElem.firstElementChild || docElem.firstChild
     // fakeBody required for <FF4 when executed in <head>
     , fakeBody = doc.createElement('body')
-    , div = doc.createElement('div');
+    , div = doc.createElement('div')
 
-  div.id = 'mq-test-1';
-  div.style.cssText = "position:absolute;top:-100em";
-  fakeBody.style.background = "none!important";
-  fakeBody.appendChild(div);
+  div.id = 'mq-test-1'
+  div.style.cssText = "position:absolute;top:-100em"
+  fakeBody.style.background = "none"
+  fakeBody.appendChild(div)
 
-  var matchMedia = window.matchMedia || function(q) {
-    div.innerHTML = '&shy;<style media="'+q+'">#mq-test-1{width:42px;}</style>';
-    docElem.insertBefore(fakeBody, refNode);
-    bool = div.offsetWidth === 42;
-    docElem.removeChild(fakeBody);
+  var matchMedia = window.matchMedia || function (q) {
+    div.innerHTML = '&shy;<style media="'+q+'">#mq-test-1{width:42px;}</style>'
+    docElem.insertBefore(fakeBody, refNode)
+    bool = div.offsetWidth === 42
+    docElem.removeChild(fakeBody)
 
-    return { matches: bool, media: q };
-  };
+    return { matches: bool, media: q }
+  }
 
-  return matchMedia;
-});
+  return matchMedia
+})
+
+
